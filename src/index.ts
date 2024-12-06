53bc8e2554557205944406074f42284117c1dd3f
“Harsh <harshwalia449@yahoo.com>
2024-12-07 00:52:21 +0530

    added room logic for websocket chat room

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
+type request = {
+    type: String,
+    payload: {
+        msg: String
+        roomId: String
+    }
+}
+type Message = {
+    room: String,
+    socket: WebSocket
+}
+
+
+const userMap = new Map<WebSocket, String>()
+
+
 wss.on('connection', (ws) => {
+
     ws.on('error', (e) => {
         console.log(e);
     })
-    ws.on('message', (data) => {
-        console.log("Data received", data.toString());
+    ws.on('message', (msg: string) => {
+        const parsedData = JSON.parse(msg);
+        if (parsedData.type === 'Join') {
+            userMap.set(ws, parsedData?.payload?.roomId)
+        }
+        if (parsedData.type === 'chat') {
+            const roomId = parsedData?.payload?.roomId
+            for (let [s, id] of userMap) {
+                if (s !== ws && id === roomId) {
+                    ws.send(parsedData?.payload?.message)
+                }
+            }
+
+        }
     })
     ws.send('something');
 })
